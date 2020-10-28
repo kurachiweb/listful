1cb5628d61a7e4b1c7f1066fdbd901280d239de9
倉地俊輔 <kurachi@kurachishunsukenoMacBook-Pro.local>
2020-10-28 10:56:01 +0900

    10/27 article update.

--- blog.config.js
+++ blog.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  url: 'https://listful.dev/',
+  url: 'https://listful.vercel.app/',
   title: 'listful',
   subtitle: 'フロントエンドエンジニア向けチートシート',
   description: 'listfulは、フロントエンドエンジニアのためのチートシート型技術ブログです。',
   author: 'Kurachi Shunsuke',
   startDate: '2020/9/27',
   analytics: '', // put your google analytics code here to add tracking
-  shareImage: 'https://listful.dev/listful-ogp.png',
+  shareImage: 'https://listful.vercel.app/listful-ogp.png',
   shareImageAlt: 'listful image.',
   twitter: '@KurachiWeb',
   postsPerPage: 10
