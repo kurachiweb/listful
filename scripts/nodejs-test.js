1d3ad1d3e30fe70258877f9ef6c752ef18d90d05
倉地俊輔 <sukuratti3408@gmail.com>
2020-12-17 07:52:39 +0900

    2020-12-17 back settings.

--- scripts/nodejs-test.js
+++ scripts/nodejs-test.js
@@ -5,3 +5,4 @@ const randomVal = crypto.randomBytes(byteLength).toString('hex');
 console.log(randomVal);
 const randomNum = parseInt(randomVal, 16);
 console.log(randomNum);
+console.log(crypto.getRandomValues(new Uint8Array(1)));
